c724bf9403aafbb71b09b5c835b51fc454789056
Yuvraj <yuvrajsinghshekhawat62@gmail.com>
2026-03-02 17:27:41 +0530

    chore: initial YuvrajOS environment setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="w-full h-full text-white flex items-center justify-center font-sans">
+      <h1>YuvrajOS Initialization Sequence Complete.</h1>
+    </div>
+  );
+};
+
+export default App;
